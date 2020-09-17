f6f77c9d450477851a2bcf036d60dff546c2fb5d
Fabio Bruni <fabiobruni@gmail.com>
2020-09-17 17:47:23 -0300

    yelp exercise

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,38 @@
+class RestaurantsController < ApplicationController
+  
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  # private
+  # # Use callbacks to share common setup or constraints between actions.
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+end
